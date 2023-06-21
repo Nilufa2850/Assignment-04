b07ff17b0e5b00834e0f83f551435413ba356b48
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-21 19:26:42 +0530

    Create Question-09.C

---
+++ Question-09.C
@@ -0,0 +1,11 @@
+// Write a program to print cubes of the first 10 natural numbers.
+
+#include<stdio.h>
+int main()
+{
+    int x ;
+    for(x=1 ; x<=10 ; x++)
+        printf("%d\n",x*x*x);
+
+    return 0;
+}
