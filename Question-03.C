d3d1c1c4498faab2849c0e554fb158e86f7bc7b5
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-21 19:06:35 +0530

    Create Question-03.C

---
+++ Question-03.C
@@ -0,0 +1,11 @@
+//Write a program to print the first 10 natural numbers in reverse order.
+
+#include<stdio.h>
+int main()
+{
+    int x ;
+    for(x=10 ; x>=1 ; x--)
+        printf("\n%d",x);
+
+    return 0;
+}
