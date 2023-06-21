3790cbafc6ca3cfb8fd9f6610818823c4fc066dd
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-21 19:24:20 +0530

    Create Question-08.C

---
+++ Question-08.C
@@ -0,0 +1,11 @@
+// Write a program to print squares of the first 10 natural numbers.
+
+#include<stdio.h>
+int main()
+{
+    int x ;
+    for(x=1 ; x<=10 ; x++)
+        printf("%d*%d = %d\n",x,x,x*x);
+  
+    return 0;
+}
