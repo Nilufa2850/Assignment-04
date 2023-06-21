6478b50adccba035884baf225f00b4fa25794d00
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-21 19:18:39 +0530

    Create Question-06.C

---
+++ Question-06.C
@@ -0,0 +1,13 @@
+// Write a program to print the first 10 even natural numbers.
+
+#include<stdio.h>
+int main()
+{
+    int x ;
+    for(x=2 ; x<=20 ; x+=2)
+        printf("%d\n",x);
+
+    return 0;
+
+    
+}
