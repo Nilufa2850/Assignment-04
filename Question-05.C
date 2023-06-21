62739a200075a711f6b8bbc7e5708c5115e8f20c
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-21 19:16:41 +0530

    Create Question-05.C

---
+++ Question-05.C
@@ -0,0 +1,16 @@
+// Write a program to print the first 10 odd natural numbers in reverse order.
+
+#include<stdio.h>
+int main()
+{
+    int x = 19 ;
+    while( x >= 1 )
+    {   
+        printf("\n%d",x);
+        x-=2 ;
+    }
+
+    return 0;
+
+    
+}
