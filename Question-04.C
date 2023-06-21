f4916e4c8dee3d423c05b5158667b8f850a9052f
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-21 19:14:09 +0530

    Create Question-04.C

---
+++ Question-04.C
@@ -0,0 +1,14 @@
+// Write a program to print the first 10 odd natural numbers.
+
+#include<stdio.h>
+int main()
+{
+    int x = 1 ;
+    while( x <= 19 )
+    {   
+        printf("\n%d",x);
+        x+=2 ;
+    }
+
+    return 0;
+}
