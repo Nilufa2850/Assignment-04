d3395184db8aa85b18daa2638465365b5b2b2b34
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-21 19:02:51 +0530

    Create Question-01.C

---
+++ Question-01.C
@@ -0,0 +1,11 @@
+//Write a program to print MySirG 5 times on the screen.
+
+#include<stdio.h>
+int main()
+{
+    int x ;
+    for(x=1 ; x<=5 ; x++)
+        printf("\nMySirG");
+
+    return 0;
+}
