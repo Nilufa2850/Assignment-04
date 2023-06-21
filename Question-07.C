d9047f29e2c76f888c54756afda780d442077bf0
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-21 19:19:57 +0530

    Create Question-07.C

---
+++ Question-07.C
@@ -0,0 +1,11 @@
+// Write a program to print the first 10 even natural numbers in reverse order.
+
+#include<stdio.h>
+int main()
+{
+    int x ;
+    for(x=20 ; x>=2 ; x-=2)
+        printf("%d\n",x);
+
+    return 0;  
+}
